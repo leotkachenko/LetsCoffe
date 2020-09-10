7c1e3fe9bfd5423c2e7d45cc883ec08abbe08f39
Леонид Ткаченко <leonidtkacenko@MacBook-Pro-Leonid.local>
2020-09-10 16:12:46 +0200

    fix lint

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,4 @@
 module.exports = {
   TOKEN: '809514746:AAHoVU1KTlJ_h31g19dMSBpVF7yCxPhlciU',
-  DB_URL: 'mongodb://localhost/youtube-video'
-}
+  DB_URL: 'mongodb://localhost/youtube-video',
+};
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
+const TelegramBot = require('node-telegram-bot-api');
 
-const TelegramBot = require('node-telegram-bot-api')
-
-const config = require('./config/env')
-const log = require('./config/function-helpers-1.0.0')
+const config = require('./config/env');
+const log = require('./config/function-helpers-1.0.0');
 
 log.logStart();
 
 const bot = new TelegramBot(config.TOKEN, {
-    polling: true
+  polling: true,
 });
 
-bot.on('message', msg => {
-
-    console.log(msg.from.first_name, msg)
+bot.on('message', (msg) => {
+  console.log(msg.from.first_name, msg);
 });
